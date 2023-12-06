3ee51a85222d72d4f221aacb48ec1aaa08ed478b
mihaipaulorban <mihaipaulorban@gmail.com>
2023-12-06 16:27:27 +0100

    feat: Titolo e Jumbotron in due componenti

---
+++ src/components/FirstComponent.vue
@@ -0,0 +1,10 @@
+<script>
+export default {
+  name: "Titolo",
+};
+</script>
+<template>
+  <header>
+    <h1>Hello World in Vue and</h1>
+  </header>
+</template>
---
+++ src/components/Jumbotron.vue
@@ -0,0 +1,14 @@
+<script>
+export default {
+  name: "Jumbotron",
+  data() {
+    return {
+      text: "Questo é il Jumbotron del mio sito in vite",
+    };
+  },
+};
+</script>
+
+<template>
+  <h2>{{ text }}</h2>
+</template>
